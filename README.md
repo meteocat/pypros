4323dd6854ead28770a3735e47ffe264618c6390
Roger Veciana <rveciana@gmail.com>
2018-10-02 14:26:35 +0200

    Prepared for Conda and first publish

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 PyPROS: Probability of Rain or Snow
 ===================================
 
-The PyPROS library applied several algorithms to calculate the probability that the precipitation will be in rain, sleet or snow, given the temperature, dew point or other magnitudes.
+The PyPROS library applied several algorithms to calculate the probability that the precipitation will be in rain, sleet or snow, given the temperature, dew point, reflectivity or other magnitudes.
+
--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ import setuptools
 setuptools.setup(
     name="pypros",
     version="0.0.1",
-    description="pypros: A small example package",
+    description="pypros: Probability of rain or snow calculation",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    install_requires=['numpy'],
     classifiers=[
-        'Development Status :: 1 - Planning', 'Intended Audience :: Science/Research',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Cython'
-    ],
+        'Programming Language :: Python :: 3.6'],
 )
