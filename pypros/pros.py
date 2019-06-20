2c3188adfb0bdcc6b1c2260f403d2ad232c855c3
ecasellas <enric.casellas@gmail.com>
2019-06-20 17:21:59 +0200

    Changes osgeo

--- pypros/pros.py
+++ pypros/pros.py
@@ -2,7 +2,8 @@
 For a point or numpy arrays
 '''
 import numpy as np
-from osgeo import gdal, osr
+import gdal
+import osr
 from pypros.psychrometrics import ttd2tw
 from pypros.psychrometrics import get_tw_sadeghi
 from pypros.ros_methods import calculate_koistinen_saltikoff
